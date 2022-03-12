295aeb910ba67f4e719167e328d6d0ed82140dbb
MRPYTHONMD <ericsalinas0701@gmail.com>
2022-03-11 21:33:44 -0500

    Se crea controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,25 @@
+//ENTRADA==VARIABLES==CONSTANTES 
+let nombreProducto='2ED. SPIDERMAN BAJO LA MASCARA'
+let precioProducto="40000"
+let pesoProducto=18.5
+let tiendeDescuento=true 
+let descripcionProducto="batimovil a escala "
+let telefonoVendedor="3005704967"
+
+const  NOMBRE_CLIENTE="ERICSON"
+
+//ARREGLOS UNIDIMENCIONALES (VECTORES)
+// SIRVEN PARA ALMACENAR VARIOS ELEMENTOS EN UNA SOLA VARIABLE 
+let productos= ["Comic " , "Camiseta " , " Gorra " , "LLavero "]
+let productos2= Array("Comic " , "Camiseta " , " Gorra " , "LLavero ")
+
+
+
+//SALIDAS 
+console.log("buenas")
+console.log(nombreProducto)
+console.log("su nombre es: "+ NOMBRE_CLIENTE + ". Su producto es:  "+nombreProducto)
+console.log(`su nombre es : ${NOMBRE_CLIENTE} y su producto es ${nombreProducto}`)
+console.log("******")
+console.log(productos)
+console.log(productos2[3])
